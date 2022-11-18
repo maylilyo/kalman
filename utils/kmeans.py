056c8ec1ba52de12c309090c64439cdeb7184bde
maylilyo <maylily_o@naver.com>
2022-11-18 11:24:21 +0900

    kmeans algorithm

---
+++ utils/kmeans.py
@@ -0,0 +1,13 @@
+import sklearn.cluster as cluster
+
+def clustering(df, methods, n_cluster):
+    if methods == "kmeans":
+        model = cluster.KMeans(n_cluster, n_init=100, max_iter=1000)
+        label = model.fit_predict(df)
+        print(f"{n_cluster} of cluster score : {model.score(df)}")
+        centroids = model.cluster_centers_
+        return label, centroids, n_cluster
+
+def find_centroid(df, methods, n_cluster):
+    _, centroids, _ = clustering(df, methods, n_cluster)
+    return centroids
