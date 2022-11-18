7f92c213cb6816f3a6060c85e286caa9e88554a1
maylilyo <maylily_o@naver.com>
2022-11-18 15:51:04 +0900

    ADD : average 값도 확인할 수 있도록 연결

--- mainstream.py
+++ mainstream.py
@@ -1,7 +1,7 @@
 from utils.make_kalman_base import make_base
-from kalman_filter import kalman_filter_scala, kalman_filter_vector
+from kalman_filter import kalman_filter_scala, kalman_filter_vector, average
 from utils.nearest_word import find_nearest_word
-from utils.kmeans import clustering
+import pandas as pd
 
 
 def main():
@@ -10,7 +10,9 @@ def main():
     n_cluster = 16
     n_word = 10  # number of catch nearest word
     methods = "kmeans"
+    average_years = 5  # 최근 n년 연도의 평균과 비교할 때 사용하는 n의 수
 
+    # origin_df = pd.read_csv("../dataset_timeseries/kr_en_v.csv") # 전체 데이터셋
     # data preprocessing
     (
         scala_motion_controls,
@@ -21,10 +23,14 @@ def main():
 
     # scala kalman filter (문서 개수) => 추후 시계열로 동작 예정
     kalman_filter_scala(scala_motion_controls, scala_measurements)
+    average(scala_measurements, average_years, "scala")
+
     # vector kalman filter (좌표)
     kalman_filter_vector(vector_motion_controls, vector_measurements)
+    average(vector_measurements, average_years, "vector")
+
     # centroid로 nearest words, nearest docx 탐색 (이거 2중for문 말고 numpy로 한번에 연산 필요)
-    find_nearest_word(n_cluster, n_word)
+    # find_nearest_word(n_cluster, n_word)
 
     # nearest docx와 nearest words 사이의 좌표 비교
 
